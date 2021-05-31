a1e12db406daab36c21fdf4cbaf16fc427b66141
iamyajat <myajat21@gmail.com>
2021-05-31 17:31:13 +0530

    feat: open whatsapp chats with phone number

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-const container = document.querySelector(".container")
-
-
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
 
-  document.addEventListener("DOMContentLoaded", showCoffees)
+function onClick() {
+  url = "https://wa.me/" + document.getElementById("phone").value + "/"
+  window.open(url);
+}
+
+document.getElementById("submit").addEventListener("click", onClick)
