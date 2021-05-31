b4951477f678d049a3b3eb384dee09e12e044e07
iamyajat <myajat21@gmail.com>
2021-05-31 16:43:10 +0530

    feat: caching added

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container")
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
+
+  document.addEventListener("DOMContentLoaded", showCoffees)
---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
