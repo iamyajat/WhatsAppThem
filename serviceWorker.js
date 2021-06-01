9b79b8c8453eaed2ff9c11407a91aba0da1b7eb1
iamyajat <myajat21@gmail.com>
2021-06-01 13:10:56 +0530

    fix: padding

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,15 @@
-const staticDevCoffee = "wathem-site-v1"
+const staticWAThem = "wathem-site-v1"
 const assets = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
+  "/images/nav-logo.png"
 ]
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticWAThem).then(cache => {
       cache.addAll(assets)
     })
   )
